07bef69f68ea0056932792d1bc5e9f2f6ca5c639
UMASHANKAR S <umashankars111333@gmail.com>
2021-01-12 20:13:30 +0530

    Signup page Building

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -1,80 +1,99 @@
 <template>
-  <div class="home" align="center" style="border:2px solid black;height:100%">
-    <v-card max-width="374px" style="margin-top: 180px">
-      <v-card-title class="display-1 justify-center blue--text font-weight-bold">
-        <span>Zen App</span>
-      </v-card-title>
+  <div class="Signup">
+    <template>
+      <v-stepper v-model="e1">
+        <v-stepper-header>
+          <v-stepper-step :complete="e1 > 1" step="1">
+            Organization Data
+          </v-stepper-step>
 
-      <v-card-text class="pb-0 pt-0">
-        <v-input class="red--text" v-show="invalidUser">
-          Invalid username or password
-        </v-input>
-        <v-text-field label="User Name" class="mt-0" v-model="userName" :rules="userValidation"></v-text-field>
-        <v-text-field label="Password" class="mt-5" v-model="password" :rules="passwordValidation"></v-text-field>
-      </v-card-text>
+          <v-divider></v-divider>
 
-      <v-card-actions class="justify-center pt-3">
-        <v-btn color="blue" class="mb-2" @click="login">
-          <v-icon>mdi-login</v-icon>
-          <span>Register</span>
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </div>
-</template>
+          <v-stepper-step :complete="e1 > 2" step="2">
+            Personal Data
+          </v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step step="3">
+            Address
+          </v-stepper-step>
+        </v-stepper-header>
+
+        <v-stepper-items>
+          <v-stepper-content step="1">
+            <v-text-field label="User Name" v-model="name"></v-text-field>
+            <v-text-field label="Email" v-model="email"></v-text-field>
+            <v-select :items="['Developer', 'Tester']" v-model="defaultRoleSelected"></v-select>
+            <v-text-field label="Password"></v-text-field>
+            <v-text-field label="Confirm Password"></v-text-field>
+
+            <v-btn color="primary" @click="e1 = 2">
+              Continue
+            </v-btn>
+
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
+
+          <v-stepper-content step="2">
+            <v-text-field label="Personal Number"></v-text-field>
+            <v-text-field label="Work Number"></v-text-field>
+            <v-text-field label="Personal Mail"></v-text-field>
+            <label for="Profile Pic">Profile Pic</label><br />
+            <input class="mb-5" type="file" @change="onFileSelected" /> <br />
+
+            <v-btn color="primary" @click="e1 = 3">
+              Continue
+            </v-btn>
+
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
+
+          <v-stepper-content step="3">
+            <v-text-field label="Address"></v-text-field>
+            <v-text-field label="Distict"></v-text-field>
+            <v-text-field label="Pincode"></v-text-field>
+
+            <v-btn color="primary" @click="e1 = 1">
+              Continue
+            </v-btn>
+
+            <v-btn text>
+              Cancel
+            </v-btn>
+          </v-stepper-content>
+        </v-stepper-items>
+      </v-stepper>
+    </template>
+  </div></template
+>
 
 <script>
 export default {
-  name: "Home",
   data() {
     return {
-      invalidUser: "",
-      userName: "",
-      password: "",
-      regX: /^[a-zA-Z]+@finastra.com$/,
-      value: "",
-      result: "",
-      users: [
-        { email: "uma@finastra.com", password: "123456" },
-        { email: "kiran@finastra.com", password: "123456" },
-        { email: "shiva@finastra.com", password: "123456" },
-      ],
-
-      userValidation: [
-        (value) => !!value || "Required.",
-        (value) => value && console.log(this.userName, this.regX),
-        (value) => (value && this.regX.test(this.userName)) || "Please enter your Organisation email",
-      ],
-      passwordValidation: [(value) => !!value || "Required", (value) => (value && value.length >= 6) || "Min 6 characters"],
+      e1: 1,
+      defaultRoleSelected: "Developer",
     };
   },
-  components: {},
   methods: {
-    login() {
-      this.users.forEach((userData) => {
-        if (userData.email == this.userName && userData.password == this.password) {
-          this.invalidUser = "user found";
-          this.$router.push({ name: "Dashboard", params: { username: this.userName } });
-        }
-        if (!this.invalidUser) {
-          this.invalidUser = "Login Failed";
-        }
-
-        // this.result = this.userName == "uma@finastra.com" ? this.$router.push({ name: "Dashboard", params: { username: this.userName } }) : (this.invalidUser = "Login Failed");
-      });
+    onFileSelected() {
+      console.log("File Selected");
     },
   },
 };
 </script>
 
-<style scoped>
-.v-input.red--text {
-  font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
+<style>
+.v-text-field {
+  width: 300px;
 }
 
-.home {
-  background-image: url("../assets/home.jpg");
-  background-repeat: no-repeat;
-  background-size: cover;
+.select {
+  widows: 300px;
 }
 </style>
